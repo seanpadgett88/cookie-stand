123e0042a29271819c9e9ae34f0b8248278132ff
Sean Padgett <sean.padgett88@yahoo.com>
2017-08-14 21:26:19 -0700

    Started on HTML and styling

--- app.js
+++ app.js
@@ -1,27 +1,118 @@
 'use strict';
 
 var firstPikeArray = [];
+var seaTacArray = [];
+var seattleCenterArray = [];
+var capitolHillArray = [];
+var alkiArray = [];
+
 var firstPike = {
-  name = '1st and Pike',
-  min = 23,
-  max = 65,
-  average = 6.3
+ min: 23,
+ max: 65,
+ average: 6.3,
 };
-function generateRandom(obj, array) {
+
+function generateRandom() {
  for (var i = 6; i < 12; i++) {
-   var randomCust = Math.floor(Math.random() * (obj.max - obj.min) + obj.min);
-   array.push(i + 'am: ' + (randomCust * obj.avgCust) + ' cookies');
+   var customerOne = Math.floor(Math.random() * (firstPike.max - firstPike.min) + firstPike.min);
+   firstPikeArray.push(i + 'AM: ' + Math.round((customerOne * firstPike.average)) + ' cookies');
  }
 
- array.push('12pm: ' + (randomCust * obj.avgCust) + ' cookies');
+ firstPikeArray.push('12PM: ' + Math.round(customerOne * firstPike.average) + ' cookies');
 
- var i = 1;
-
- for (var i = 1; i < 9; i++) {
-   var randomCust = Math.floor(Math.random() * (obj.max - obj.min) + obj.min);
-   array.push(i + 'pm: ' + (randomCust * obj.avgCust) + ' cookies');
+ for (var j = 1; j < 9; j++) {
+   var customerTwo = Math.floor(Math.random() * (firstPike.max - firstPike.min) + firstPike.min);
+   firstPikeArray.push(j + 'pm: ' + Math.round(customerTwo * firstPike.average) + ' cookies');
  }
- return[array];
+ generateRandom(firstPike, firstPikeArray);
+ return[firstPikeArray];
+
+}
+var seaTac = {
+ min: 3,
+ max: 24,
+ average: 1.2,
+};
+
+function generateRandomOne() {
+ for (var i = 6; i < 12; i++) {
+   var customerThree = Math.floor(Math.random() * (seaTac.max - seaTac.min) + seaTac.min);
+   seaTacArray.push(i + 'AM: ' + Math.round((customerThree * seaTac.average)) + ' cookies');
+ }
+
+ seaTacArray.push('12PM: ' + Math.round(customerThree * seaTac.average) + ' cookies');
+
+
+ for (var j = 1; j < 9; j++) {
+   var customerFour = Math.floor(Math.random() * (seaTac.max - seaTac.min) + seaTac.min);
+   seaTacArray.push(j + 'pm: ' + Math.round(customerFour * seaTac.average) + ' cookies');
+ }
+ generateRandomOne(seaTac, seaTacArray);
+ return[seaTacArray];
 }
 
-generateRandom(firstAndPike, salesFirstAndPike);
+var seattleCenter = {
+ min: 23,
+ max: 65,
+ average: 6.3,
+};
+
+function generateRandomTwo() {
+ for (var i = 6; i < 12; i++) {
+   var customerFive = Math.floor(Math.random() * (seattleCenter.max - seattleCenter.min) + seattleCenter.min);
+   seattleCenterArray.push(i + 'AM: ' + Math.round((customerFive * seattleCenter.average)) + ' cookies');
+ }
+
+ seattleCenterArray.push('12PM: ' + Math.round(customerFive * seattleCenter.average) + ' cookies');
+
+
+ for (var j = 1; j < 9; j++) {
+   var customerSix = Math.floor(Math.random() * (seattleCenter.max - seattleCenter.min) + seattleCenter.min);
+   seattleCenterArray.push(j + 'pm: ' + Math.round(customerSix * seattleCenter.average) + ' cookies');
+ }
+ generateRandomTwo(seattleCenter, seattleCenterArray);
+ return[seattleCenterArray];
+
+ var capitolHill = {
+  min: 23,
+  max: 65,
+  average: 6.3,
+ };
+
+ function generateRandomThree() {
+  for (var i = 6; i < 12; i++) {
+    var customerSeven = Math.floor(Math.random() * (capitolHill.max - capitolHill.min) + capitolHill.min);
+    capitolHillArray.push(i + 'AM: ' + Math.round((customerSeven * capitolHill.average)) + ' cookies');
+  }
+
+capitolHillArray.push('12PM: ' + Math.round(customerSeven * capitolHill.average) + ' cookies');
+
+
+  for (var j = 1; j < 9; j++) {
+    var customerEight = Math.floor(Math.random() * (capitolHill.max - capitolHill.min) + capitolHill.min);
+    capitolHillArray.push(j + 'pm: ' + Math.round(customerEight * capitolHill.average) + ' cookies');
+  }
+  generateRandomThree(firstPike, firstPikeArray);
+  return[capitolHillArray];
+
+  var alki = {
+   min: 23,
+   max: 65,
+   average: 6.3,
+  };
+
+  function generateRandomFour() {
+   for (var i = 6; i < 12; i++) {
+     var customerNine = Math.floor(Math.random() * (alki.max - alki.min) + alki.min);
+     alkiArray.push(i + 'AM: ' + Math.round((customerNine * alki.average)) + ' cookies');
+   }
+
+   alkiArray.push('12PM: ' + Math.round(customerNine * alki.average) + ' cookies');
+
+
+   for (var j = 1; j < 9; j++) {
+     var customerTen = Math.floor(Math.random() * (alki.max - alki.min) + alki.min);
+     alkiArray.push(j + 'pm: ' + Math.round(customerTen * alki.average) + ' cookies');
+   }
+   generateRandomFour(alki, alkiArray);
+   return[alkiArray];
