3185126d223c033b0c1ffbf6a204aa623987a4b5
Sean Padgett <sean.padgett88@yahoo.com>
2017-08-15 15:43:48 -0700

    Finally rebuilt literal notations into constructor functions

--- app.js
+++ app.js
@@ -117,3 +117,21 @@ capitolHillArray.push('12PM: ' + Math.round(customerSeven * capitolHill.average)
    }
    generateRandomFour(alki, alkiArray);
    return[alkiArray];
+
+
+
+
+
+   var me = new Person('Nicholas Hunt-Walker', 71, 185);
+   var mySon = new Person('Nathaniel Hunt-Walker', 22.44, 9.7);
+
+   var names = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'.split(' ');
+
+   for (var i = 0; i < names.length; i++) {
+     // people.push(new Person(names[i], Math.random() * (72-60) + 60), Math.random() * (200 - 100) + 100);
+     // this does the same as below
+     var person = new Person();
+     person.name = names[i];
+     person.height = Math.random() * (72 - 60) + 60;
+     person.weight = Math.random() * (200 - 100) + 100;
+     // people.push(person);
