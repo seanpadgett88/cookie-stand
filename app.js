80497f905cfd0db285834b1a889a8b92e2f3c2db
Sean Padgett <sean.padgett88@yahoo.com>
2017-08-15 16:06:00 -0700

    It's printing now

--- app.js
+++ app.js
@@ -1,137 +1,36 @@
 'use strict';
 
-var firstPikeArray = [];
-var seaTacArray = [];
-var seattleCenterArray = [];
-var capitolHillArray = [];
-var alkiArray = [];
+  function Store (name, minCust, maxCust, avgCookies) {
+    this.storeHours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
+    this.name = name;
+    this.minCust = minCust;
+    this.maxCust = maxCust;
+    this.avgCookies = avgCookies;
+    this.simCookies = [];
+    this.totalCookieSales = 0;
 
-var firstPike = {
- min: 23,
- max: 65,
- average: 6.3,
-};
+    this.randomCustomersPerHour = function(){
+      return Math.floor(Math.random() * (this.maxCust - this.minCust + 1) + this.minCust);
+     },
 
-function generateRandom() {
- for (var i = 6; i < 12; i++) {
-   var customerOne = Math.floor(Math.random() * (firstPike.max - firstPike.min) + firstPike.min);
-   firstPikeArray.push(i + 'AM: ' + Math.round((customerOne * firstPike.average)) + ' cookies');
- }
-
- firstPikeArray.push('12PM: ' + Math.round(customerOne * firstPike.average) + ' cookies');
-
- for (var j = 1; j < 9; j++) {
-   var customerTwo = Math.floor(Math.random() * (firstPike.max - firstPike.min) + firstPike.min);
-   firstPikeArray.push(j + 'pm: ' + Math.round(customerTwo * firstPike.average) + ' cookies');
- }
- var sumOfBoth = customerOne + customerTwo;
- generateRandom(sumOfBoth, firstPikeArray);
- return[firstPikeArray];
-
-}
-var seaTac = {
- min: 3,
- max: 24,
- average: 1.2,
-};
-
-function generateRandomOne() {
- for (var i = 6; i < 12; i++) {
-   var customerThree = Math.floor(Math.random() * (seaTac.max - seaTac.min) + seaTac.min);
-   seaTacArray.push(i + 'AM: ' + Math.round((customerThree * seaTac.average)) + ' cookies');
- }
-
- seaTacArray.push('12PM: ' + Math.round(customerThree * seaTac.average) + ' cookies');
-
-
- for (var j = 1; j < 9; j++) {
-   var customerFour = Math.floor(Math.random() * (seaTac.max - seaTac.min) + seaTac.min);
-   seaTacArray.push(j + 'pm: ' + Math.round(customerFour * seaTac.average) + ' cookies');
- }
- generateRandomOne(seaTac, seaTacArray);
- return[seaTacArray];
-}
-
-var seattleCenter = {
- min: 23,
- max: 65,
- average: 6.3,
-};
-
-function generateRandomTwo() {
- for (var i = 6; i < 12; i++) {
-   var customerFive = Math.floor(Math.random() * (seattleCenter.max - seattleCenter.min) + seattleCenter.min);
-   seattleCenterArray.push(i + 'AM: ' + Math.round((customerFive * seattleCenter.average)) + ' cookies');
- }
-
- seattleCenterArray.push('12PM: ' + Math.round(customerFive * seattleCenter.average) + ' cookies');
-
-
- for (var j = 1; j < 9; j++) {
-   var customerSix = Math.floor(Math.random() * (seattleCenter.max - seattleCenter.min) + seattleCenter.min);
-   seattleCenterArray.push(j + 'pm: ' + Math.round(customerSix * seattleCenter.average) + ' cookies');
- }
- generateRandomTwo(seattleCenter, seattleCenterArray);
- return[seattleCenterArray];
-
- var capitolHill = {
-  min: 23,
-  max: 65,
-  average: 6.3,
- };
-
- function generateRandomThree() {
-  for (var i = 6; i < 12; i++) {
-    var customerSeven = Math.floor(Math.random() * (capitolHill.max - capitolHill.min) + capitolHill.min);
-    capitolHillArray.push(i + 'AM: ' + Math.round((customerSeven * capitolHill.average)) + ' cookies');
+   this.eachHourSales = function(){
+    this.simCookies = [];
+    this.totalCookieSales = 0;
+    for (var i = 0; i < this.storeHours.length; i++) {
+      var hourlyCookieSales = Math.ceil(this.avgCookies * this.randomCustomersPerHour());
+      this.simCookies.push(hourlyCookieSales);
+      this.totalCookieSales += hourlyCookieSales;
+      }
+   };
+   this.eachHourSales();
   }
 
-capitolHillArray.push('12PM: ' + Math.round(customerSeven * capitolHill.average) + ' cookies');
+  var pikeFirst = new Store('Pike and First', 23, 65, 6.3);
+  var seaTac = new Store('SeaTac', 3, 24, 1.2);
+  var seattleCenter = new Store('Seattle Center', 11, 38, 3.7);
+  var capitolHill = new Store('Capitol Hill', 20, 38, 2.3);
+  var alki = new Store('Alki', 2, 16, 4.6);
 
-
-  for (var j = 1; j < 9; j++) {
-    var customerEight = Math.floor(Math.random() * (capitolHill.max - capitolHill.min) + capitolHill.min);
-    capitolHillArray.push(j + 'pm: ' + Math.round(customerEight * capitolHill.average) + ' cookies');
-  }
-  generateRandomThree(firstPike, firstPikeArray);
-  return[capitolHillArray];
-
-  var alki = {
-   min: 23,
-   max: 65,
-   average: 6.3,
-  };
-
-  function generateRandomFour() {
-   for (var i = 6; i < 12; i++) {
-     var customerNine = Math.floor(Math.random() * (alki.max - alki.min) + alki.min);
-     alkiArray.push(i + 'AM: ' + Math.round((customerNine * alki.average)) + ' cookies');
-   }
-
-   alkiArray.push('12PM: ' + Math.round(customerNine * alki.average) + ' cookies');
-
-
-   for (var j = 1; j < 9; j++) {
-     var customerTen = Math.floor(Math.random() * (alki.max - alki.min) + alki.min);
-     alkiArray.push(j + 'pm: ' + Math.round(customerTen * alki.average) + ' cookies');
-   }
-   generateRandomFour(alki, alkiArray);
-   return[alkiArray];
-
-
-
-
-
-   var me = new Person('Nicholas Hunt-Walker', 71, 185);
-   var mySon = new Person('Nathaniel Hunt-Walker', 22.44, 9.7);
-
-   var names = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'.split(' ');
-
-   for (var i = 0; i < names.length; i++) {
-     // people.push(new Person(names[i], Math.random() * (72-60) + 60), Math.random() * (200 - 100) + 100);
-     // this does the same as below
-     var person = new Person();
-     person.name = names[i];
-     person.height = Math.random() * (72 - 60) + 60;
-     person.weight = Math.random() * (200 - 100) + 100;
-     // people.push(person);
+for (var i = 0; i < seaTac.storeHours.length; i++) {
+  console.log(seaTac.simCookies[i]);
+}
